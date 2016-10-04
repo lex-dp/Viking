32a0a618da34e7bbd8632e906d0fa5f1e5e53b17
Alexey Kapitonenko <kapitonenko5@gmail.com>
2016-10-04 13:55:45 +0300

    Almost done Prices page

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,32 @@
 $(document).ready(function() {
-	
 
 	
-	//Plugin slick 
-	$('.header-slider').slick({
-	  dots: true,
-	  speed: 300,
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  autoplay: true,
-	  swipeToSlide: true,
-	  pauseOnHover: false,
-	  autoplaySpeed: 2500,
-	  responsive: [
-		{
-		  breakpoint: 1024,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			dots: true
-		  }
-		},
-		{
-		  breakpoint: 931,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1
-		  }
-		},
-		{
-		  breakpoint: 480,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1
-		  }
-		}
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
-	  ]
-	});
-	
-	//Arrows
-	$(this).find("button").hide();
-	$(".slick-slider").hover(function(){
-		$(this).find("button").show(400);
-	},function(){
-		$(this).find("button").hide(400);	
-	});
-	//End of slick
 	
 	$("#tabbedPanel").tabs({
-		beforeActivate: function(evt) {
-			location.hash=$(evt.currentTarget).attr('href');
-		},
-		active: 1,
+		active: 1,		
 		event: "mouseover"
-		
 	});
+	
+	$("#tabbedPanelCallery , #prices").tabs({
+		beforeActivate : function(evt) {
+			location.hash=$(evt.currentTarget).attr('href');
+		}
+	});
+	var hash = location.hash; 
+    if (hash) { 
+		$('#tabbedPanelCallery').tabs("load", hash);
+		
+	} 
+	
 
 	
-	
+
 	
 
 	$( window ).resize(function() {
 		var $width = $(window).width();
 		console.log($width);
 	});
-	
-	
-	
-	
-	
-	
-
 	
 	
 });  
